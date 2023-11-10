8b39fa40a3e44ebfaf9efdd6e0e5329eb8a41974
MichaelOE <37800126+MichaelOE@users.noreply.github.com>
2023-11-10 14:52:49 +0100

    Update __init__.py

--- custom_components/stecagrid/__init__.py
+++ custom_components/stecagrid/__init__.py
@@ -2,6 +2,8 @@
 import asyncio
 import logging
 import requests
+import socket
+import struct
 from datetime import timedelta, datetime
 import voluptuous as vol
 from homeassistant.util import Throttle
@@ -16,11 +18,12 @@ CONFIG_SCHEMA = vol.Schema({DOMAIN: vol.Schema({})}, extra=vol.ALLOW_EXTRA)
 
 PLATFORMS = ["sensor"]
 
-MIN_TIME_BETWEEN_UPDATES = timedelta(seconds=15)
+MIN_TIME_BETWEEN_UPDATES = timedelta(seconds=1)
 
 
 async def async_setup(hass: HomeAssistant, config: dict):
     """Set up the StecaGrid component."""
+
     hass.data[DOMAIN] = {}
     return True
 
@@ -29,8 +32,8 @@ async def async_setup(hass: HomeAssistant, config: dict):
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Set up StecaGrid from a config entry."""
-    inverter_host = entry.data['inverter_host'] #inverter_host
-    inverter_port = entry.data['inverter_port'] #inverter_port
+    inverter_host = entry.data['inverter_host']
+    inverter_port = entry.data['inverter_port']
     
     hass.data[DOMAIN][entry.entry_id] = HassStecaGrid(inverter_host, inverter_port)
 
@@ -61,34 +64,43 @@ class HassStecaGrid:
     def __init__(self, inverter_host, inverter_port):
         self._inverter_host = inverter_host
         self._inverter_port = inverter_port
+        _LOGGER.debug("Stecagrid __init__" + self._inverter_host)
 
-    @Throttle(MIN_TIME_BETWEEN_UPDATES)
-    def update_energy(self):
+    def get_name(self):
+        return f"steca_grid_{self._inverter_host}_{str(self._inverter_port)}"
+
+    def get_unique_id(self):
+        return f"steca_grid_power_{self._inverter_host}_{str(self._inverter_port)}"
+
+    #@Throttle(MIN_TIME_BETWEEN_UPDATES)
+    def update_power(self):
         _LOGGER.debug("Fetching energy data from Stecagrid")
 
-        #host = '10.0.10.159'
-        #port = 2323
+        power_output = 0.0
+
+        host = self._inverter_host # '10.0.10.159'
+        port = self._inverter_port # 2323
         msgRequest = b'\x02\x01\x00\x10\x01\xC9\x65\x40\x03\x00\x01\x29\x7E\x29\xBE\x03'
 
-        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        client.connect((self._inverter_host, self._inverter_port))
-        client.send(msgRequest)
+        tcpClient = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        tcpClient.connect((host, port))
+        tcpClient.send(msgRequest)
 
-        msgResponse = client.recv(30)
-        #msgResponse = b'\x02\x01\x00\x1e\xc9\x01\xFF\xFF\x00\x00\x0f\xFF\x00\x00\x07\xFF\xFF\xFF\xFF\xFF\xFF\xFF\x0b\x0b\x90\x00\x85\xFF\xFF\x03'
+        msgResponse = tcpClient.recv(30)
         power_output = 0.0
 
         #print (msgResponse)
 
-        if (msgResponse[22] == 0x0B):
-            temp = ((msgResponse[25] << 8 | msgResponse[23]) << 8 | msgResponse[24]) << 7
-            power_output = struct.unpack('!f', bytes.fromhex(str(hex(temp)).split('0x')[1]))[0]
-        else:
-            _LOGGER.debug("Ingen solproduktion fra Steca inverter")
+        try:
+            if (msgResponse[22] == 0x0B):
+                temp = ((msgResponse[25] << 8 | msgResponse[23]) << 8 | msgResponse[24]) << 7
+                power_output = struct.unpack('!f', bytes.fromhex(str(hex(temp)).split('0x')[1]))[0]
+                _LOGGER.debug(f"Steca grid power_output: {str(power_output)} W")
+            else:
+                _LOGGER.info("Steca grid - Ingen solproduktion")
+        except Exception:  # pylint: disable=broad-except
+            _LOGGER.info(f"Fejl ved parsing af inverterdata! - der er sikkert overskyet, output: {str(power_output)}")
 
-        #print(power_output, 'W')
-
-        #day_data = self._client.get_latest(self._metering_point)
         return power_output
-       
+
         _LOGGER.debug("Done fetching energy data from Stecagrid")
