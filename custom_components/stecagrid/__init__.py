9a9b52fe1c0d72aea48120d8180d4583d79652d4
MichaelOE <37800126+MichaelOE@users.noreply.github.com>
2024-05-16 15:33:31 +0200

    Update __init__.py

--- custom_components/stecagrid/__init__.py
+++ custom_components/stecagrid/__init__.py
@@ -62,6 +62,9 @@ class HassStecaGrid:
         self._inverter_host = inverter_host
         self._inverter_port = inverter_port
         _LOGGER.debug("Stecagrid __init__" + self._inverter_host)
+        
+        # create an instance of StecaConnector
+        self.steca_connector = steca.StecaConnector(self._inverter_host, self._inverter_port)
 
     def get_name(self):
         return f"steca_grid_{self._inverter_host}_{str(self._inverter_port)}"
@@ -71,19 +74,14 @@ class HassStecaGrid:
 
     def update_power(self):
         # create an instance of StecaConnector
-        steca_connector = steca.StecaConnector(self._inverter_host, self._inverter_port)
+        #steca_connector = steca.StecaConnector(self._inverter_host, self._inverter_port)
 
         _LOGGER.debug("Requesting timestamp from Stecagrid")
-        inverterTime = steca_connector.GetInverterTime()
+        inverterTime = self.steca_connector.GetInverterTime()
         _LOGGER.debug(f"Inverter time: {inverterTime}")
 
         _LOGGER.debug("Requesting current power_output from Stecagrid")
-        power_output = steca_connector.GetPowerOutput()
-        if isinstance(power_output, float):
-            _LOGGER.debug("power_output er en float")
-        else:
-            _LOGGER.debug("power_output er ikke en float")
-            power_output = 0.0
-
+        power_output = self.steca_connector.GetPowerOutput()
         _LOGGER.debug(f"Done fetching info from Stecagrid, {power_output}W")
+
         return power_output
