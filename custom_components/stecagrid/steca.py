4fad46611c05c15d5af085714104fecfe8a527c1
MichaelOE <37800126+MichaelOE@users.noreply.github.com>
2025-09-06 18:37:24 +0200

    Added sensors

--- custom_components/stecagrid/steca.py
+++ custom_components/stecagrid/steca.py
@@ -103,23 +103,33 @@ class StecaConnector:
         # Return the final CRC value
         return total
 
-    def formulaToFloat(self, threeBytesToConvert):
-        try:
-            temp = (
-                (threeBytesToConvert[2] << 8 | threeBytesToConvert[0]) << 8
-                | threeBytesToConvert[1]
-            ) << 7
-            power_output = struct.unpack(
-                "!f", bytes.fromhex(str(hex(temp)).split("0x")[1])
-            )[0]
-        except Exception:  # pylint: disable=broad-except
-            _LOGGER.error(
-                "Fejl ved parsing af inverterdata! - der er sikkert overskyet (output = ",
-                power_output,
-                ")",
-            )
-        return power_output
+    def formulaToFloat(self, bytesToConvert):
+        """
+        Convert 4 bytes (formula + 3 value bytes) to float.
+        Formula byte is ignored here, only used to check 0x00.
+        """
+        if len(bytesToConvert) != 4:
+            _LOGGER.warning("formulaToFloat: expected 4 bytes, got %d", len(bytesToConvert))
+            return 0.0
 
+        try:
+            formula, b0, b1, b2 = bytesToConvert
+
+            if formula == 0x00:  # value not available
+                return None
+
+            # Reconstruct 24-bit value into 32-bit float representation
+            temp_int = ((b2 << 8 | b0) << 8 | b1) << 7
+            temp_bytes = temp_int.to_bytes(4, byteorder='big', signed=False)
+
+            power_output = struct.unpack(">f", temp_bytes)[0]
+
+        except Exception as e:
+            _LOGGER.error("Error parsing inverter data: %s", e)
+            power_output = 0.0
+
+        return power_output
+        
     def formulaToSInt(self, bytesToConvert):
         try:
             sint = int.from_bytes(bytesToConvert, byteorder="big", signed=True)
@@ -132,18 +142,158 @@ class StecaConnector:
         return sint
 
     async def GetInverterTime(self):
-        req = self.GenerateRequestTelegram(b"\x04")
-        retVal = await self.PollInverter(req)
-        return retVal
+        req = self.GenerateRequestTelegram(4)
+        msg_response = await self.PollInverter(req)
 
-    async def GetPowerOutput(self):
-        req = self.GenerateRequestTelegram(b"\x29")
-        return await self.PollInverter(req)
+        _LOGGER.debug("Data and time")
+        year = self.formulaToSInt(msg_response[13:15])
+        month = self.formulaToSInt(msg_response[17:19])
+        day = self.formulaToSInt(msg_response[21:23])
+        hour = self.formulaToSInt(msg_response[25:27])
+        minute = self.formulaToSInt(msg_response[29:31])
+        second = self.formulaToSInt(msg_response[33:35])
+        _LOGGER.debug(
+            f"Date in inverter {year}-{month:02d}-{day:02d} {hour:02d}:{minute:02d}:{second:02d}"
+        )
+        self.current_timestamp = (
+            f"{year}-{month:02d}-{day:02d} {hour:02d}:{minute:02d}:{second:02d}"
+        )
+        self.timestamp_status = msg_response[39 : len(msg_response) - 4].decode(
+            "utf-8"
+        )
+        # return f"{self.timestamp_status}, status: '{msg_response[39 : len(msg_response) - 4].decode('utf-8')}'"
+        return self.current_timestamp
 
+    async def GetACOutput(self):
+        req = self.GenerateRequestTelegram(41)
+        msg_response = await self.PollInverter(req)
+        
+        _LOGGER.debug("AC power")
+        if msg_response[22] == 0x0B:
+            power_output = self.formulaToFloat(msg_response[22:26])
+
+            if (
+                power_output <= PowerOutput_MIN
+                or power_output > PowerOutput_MAX
+            ):  # Range check
+                _LOGGER.warning(
+                    f"Unusual inverter power '{power_output}', probably wrong message received from inverter"  # noqa: G004
+                )
+                power_output = PowerOutput_MIN
+
+        else:
+            _LOGGER.info("Ingen solproduktion")
+            power_output = PowerOutput_MIN
+
+        self._previous_value = power_output
+        self.current_power_output = power_output
+        return round(self.current_power_output, 1)
+    
+    async def GetDailyYield(self):
+        req = self.GenerateRequestTelegram(60)
+        msg_response = await self.PollInverter(req)
+
+        _LOGGER.debug("daily yield from inverter")
+
+        daily_yield = PowerOutput_MIN
+
+        # Find the formula byte 0x09 for CurrentDayYield
+        formula_index = msg_response.find(b'\x09')
+        if formula_index != -1 and len(msg_response) >= formula_index + 4:
+            yield_bytes = msg_response[formula_index : formula_index + 4]  # 4 bytes
+            daily_yield = self.formulaToFloat(yield_bytes)
+
+        else:
+            _LOGGER.info("Ingen solproduktion")
+
+        print("Daily Yield (Wh):", round(daily_yield, 1))
+
+        self._previous_value = daily_yield
+        self.current_power_output = daily_yield
+        return round(daily_yield, 1)
+
+    async def GetNominalPower(self):
+        req = self.GenerateRequestTelegram(29)
+        msg_response = await self.PollInverter(req)
+
+        _LOGGER.debug("Nominal AC Power from inverter")
+
+        try:
+            formula_index = msg_response.find(b'\x0b', 15)  # find first 0x0B
+            if formula_index == -1 or len(msg_response) < formula_index + 5:
+                print("Panel power bytes not found")
+                return 0.0
+
+            power_bytes = msg_response[formula_index : formula_index + 4]  # 4 bytes after 0x0B
+            power_output = self.formulaToFloat(power_bytes)
+            # print("Panel Power (W):", round(power_output, 3))
+            return round(power_output, 3)
+        
+        except Exception as e:
+            _LOGGER.error("Fejl ved parsing af inverterdata i GetPanelOutput! - der er sikkert overskyet. error=%s", e)
+            return PowerOutput_MIN
+
+    async def GetPanelOutput(self):
+        req = self.GenerateRequestTelegram(34)
+        msg_response = await self.PollInverter(req)
+        
+        try:
+            formula_index = msg_response.find(b'\x0b', 15)  # find first 0x0B
+            if formula_index == -1 or len(msg_response) < formula_index + 5:
+                print("Panel power bytes not found")
+                return 0.0
+
+            power_bytes = msg_response[formula_index : formula_index + 4]  # 4 bytes after 0x0B
+            power_output = self.formulaToFloat(power_bytes)
+            print("Panel Power (W):", round(power_output, 3))
+            return round(power_output, 3)
+        
+        except Exception as e:
+            _LOGGER.error("Fejl ved parsing af inverterdata i GetPanelOutput! - der er sikkert overskyet. error=%s", e)
+            return PowerOutput_MIN
+
+    async def GetPanelVoltage(self):
+        req = self.GenerateRequestTelegram(35)
+        msg_response = await self.PollInverter(req)
+        
+        try:
+            formula_index = msg_response.find(b'\x05', 15)  # find first 0x0B
+            if formula_index == -1 or len(msg_response) < formula_index + 5:
+                print("Panel voltage bytes not found")
+                return 0.0
+            
+            power_bytes = msg_response[formula_index : formula_index + 4]  # 4 bytes after 0x0B
+            power_output = self.formulaToFloat(power_bytes)
+            print("Panel voltage (V):", round(power_output, 3))
+            return round(power_output, 3)
+        
+        except Exception as e:
+            _LOGGER.error("Fejl ved parsing af inverterdata i GetPanelOutput! - der er sikkert overskyet. error=%s", e)
+            return PowerOutput_MIN
+
+    async def GetPanelCurrent(self):
+        req = self.GenerateRequestTelegram(36)
+        msg_response = await self.PollInverter(req)
+        
+        try:
+            formula_index = msg_response.find(b'\x07', 15)  # find first 0x0B
+            if formula_index == -1 or len(msg_response) < formula_index + 5:
+                print("Panel Current bytes not found")
+                return 0.0
+            
+            power_bytes = msg_response[formula_index : formula_index + 4]  # 4 bytes after 0x0B
+            power_output = self.formulaToFloat(power_bytes)
+            print("Panel Current (A):", round(power_output, 3))
+            return round(power_output, 3)
+        
+        except Exception as e:
+            _LOGGER.error("Fejl ved parsing af inverterdata i GetPanelOutput! - der er sikkert overskyet. error=%s", e)
+            return PowerOutput_MIN
+            
     def GenerateRequestTelegram(self, RequestIdentifier):
         ### Generate Dataframe
         DataFrame = bytearray(b"")
-        DataFrame.extend(RequestIdentifier)  # Add Identifier to frame
+        DataFrame.extend(bytes([RequestIdentifier]))  # Add Identifier to frame
         DataFrameCRC = self.frameCRC(CRC_8_OFFSET, DataFrame)  # calculate dataframe CRC
         DataFrameLength = bytearray(struct.pack(">h", len(DataFrame)))
         DataFrame.append(DataFrameCRC)  # and append
@@ -184,6 +334,7 @@ class StecaConnector:
             # Send request
             writer.write(requestMessage)
             await writer.drain()  # Ensure the message is sent
+            # print(f"TX {len(requestMessage)} bytes '{requestMessage.hex()}'")
 
             # Receive response
             msg_response = await reader.read(1024)
@@ -195,6 +346,9 @@ class StecaConnector:
             await writer.wait_closed()
 
             # return msg_response
+            print(f"RX {length} bytes '{msg_response.hex()}'")
+            # print(f"RX {length} bytes '{str(msg_response)}'")
+
 
         except Exception as e:
             _LOGGER.debug(
@@ -219,52 +373,13 @@ class StecaConnector:
                 return PowerOutput_MIN
 
             ResponseCode = msg_response[8]
-            ResponseValue = msg_response[11]
+            #ResponseValue = msg_response[11]
 
             if ResponseCode == 0x01:  # ServiceNotSupported
                 _LOGGER.warning("Service Not Supported by inverter")
                 return "Service Not Supported by inverter"
-            elif ResponseValue == 0x29:  # Nominal AC power of inverter
-                _LOGGER.debug("Nominal AC power of inverter")
-                if msg_response[22] == 0x0B:
-                    power_output = self.formulaToFloat(msg_response[23:26])
 
-                    if (
-                        power_output <= PowerOutput_MIN
-                        or power_output > PowerOutput_MAX
-                    ):  # Range check
-                        _LOGGER.warning(
-                            f"Unusual inverter power '{power_output}', probably wrong message received from inverter"  # noqa: G004
-                        )
-                        power_output = PowerOutput_MIN
-
-                else:
-                    _LOGGER.info("Ingen solproduktion")
-                    power_output = PowerOutput_MIN
-
-                self._previous_value = power_output
-                self.current_power_output = power_output
-                return round(self.current_power_output, 0)
-
-            elif ResponseValue == 0x04:  # Data and time
-                _LOGGER.debug("Data and time")
-                year = self.formulaToSInt(msg_response[13:15])
-                month = self.formulaToSInt(msg_response[17:19])
-                day = self.formulaToSInt(msg_response[21:23])
-                hour = self.formulaToSInt(msg_response[25:27])
-                minute = self.formulaToSInt(msg_response[29:31])
-                second = self.formulaToSInt(msg_response[33:35])
-                _LOGGER.debug(
-                    f"Date in inverter {year}-{month:02d}-{day:02d} {hour:02d}:{minute:02d}:{second:02d}"
-                )
-                self.current_timestamp = (
-                    f"{year}-{month:02d}-{day:02d} {hour:02d}:{minute:02d}:{second:02d}"
-                )
-                self.timestamp_status = msg_response[39 : len(msg_response) - 4].decode(
-                    "utf-8"
-                )
-                # return f"{self.timestamp_status}, status: '{msg_response[39 : len(msg_response) - 4].decode('utf-8')}'"
-                return self.current_timestamp
+            return msg_response
 
         except Exception:  # pylint: disable=broad-except
             _LOGGER.debug(
